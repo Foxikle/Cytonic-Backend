194c89cfaa84393b966f3b0fd09db1d3539fa8a4
foxikle <tristantroop260@gmail.com>
2024-04-20 16:01:49 -0400

    Add routes for new features

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,22 @@ Rails.application.routes.draw do
 
   # Editing / user panel
   patch '/usernames/edit', to: 'users/usernames#update'
+  put '/users/avatars', to: 'users/avatars#set_avatar'
+  delete '/users/avatars', to: 'users/avatars#remove_avatar'
+
+  # Reports
+  post '/reports/chat', to: 'reports/chat_reports#new'
+
+  # banners
+  post '/banners', to: 'banners#create'
+  get '/banners', to: 'banners#get_banner'
+  delete '/banners', to: 'banners#remove_banner'
 
   # Admin stuff
   patch '/admin/roles', to: 'admin/roles#set_user_role'
   get '/admin/users', to: 'admin/users#list_users'
+  post '/admin/users/terminate', to: 'admin/users#terminate_user'
+  post '/admin/users/restore', to: 'admin/users#restore_user'
+
+
 end
