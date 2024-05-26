b373863e91f2a5397b226ac8beb350137cf8d1cf
foxikle <tristantroop260@gmail.com>
2024-05-26 14:43:30 -0400

    Create models for forums

---
+++ app/models/forums/thread.rb
@@ -0,0 +1,61 @@
+class Forums::Thread < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :nullify
+  before_update :save_version
+
+
+  def soft_delete
+    self.deleted_at = DateTime.now
+    self.deleted = true
+    self.save
+  end
+
+  def lock_thread!
+    self.locked = true
+    self.locked_at = DateTime.now
+    self.save
+  end
+
+  def unlock_thread!
+    self.locked = false
+    self.locked_at = nil
+    self.save
+  end
+
+  # Define who can view a thread
+  def show? (user)
+    true unless thread_restricted? user
+  end
+
+  private
+
+  def save_version
+    # Create a new thread version with the current content and edited_at timestamp
+    ThreadVersion.create(
+      user_id: user_id,      # ID of the user who created the thread
+      body: body,            # Content of the thread before updating
+      edited_at: updated_at  # The timestamp when the thread was last edited
+    )
+  end
+
+  def thread_restricted? (user)
+    if user
+      return false if unrestricted? user.role || self.user == user
+    end
+
+    restricted_categories = ["STAFF_CONTACT"]
+    restricted_topics = %w[BUG_REPORTS PUNISHMENT_APPEALS]
+    puts self.title + " restricted? " + self.deleted
+
+    restricted_categories.include?(self.category) || restricted_topics.include?(self.topic) || self.deleted
+  end
+
+  def unrestricted?(role)
+    case role
+    when Role::OWNER, Role::ADMIN, Role::MODERATOR
+      true
+    else
+      false
+    end
+  end
+end
---
+++ app/models/thread_report.rb
@@ -0,0 +1,5 @@
+class ThreadReport < ApplicationRecord
+  belongs_to :thread, :class_name => 'Forums::Thread' # the comment in question
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id' # the user who reported the comment
+  belongs_to :user, class_name: 'User', foreign_key: 'resolving_user_id', optional: true # the user who resolved the reported comment
+end
