bf3e7df5c799e1bab51d48151deb5ab8f89905d4
foxikle <tristantroop260@gmail.com>
2024-06-04 17:38:20 -0400

    More fixes to thread_serializer.rb

--- app/serializers/thread_serializer.rb
+++ app/serializers/thread_serializer.rb
@@ -2,9 +2,11 @@ class ThreadSerializer
   include JSONAPI::Serializer
 
 
-  attributes :id, :private, :title, :body, :user, :topic, :category, :created_at, :updated_at, :deleted, :deleted_at, :edited, :deleted_at, :locked, :locked_at
-
-  belongs_to :user, serializer: SafeUserSerializer
+  attributes :id, :private, :title, :body, :topic, :category, :created_at, :updated_at, :deleted, :deleted_at, :edited, :deleted_at, :locked, :locked_at
+  attribute :user do |thread|
+    SafeUserSerializer.new(thread.user).serializable_hash
+  end
+  # belongs_to :user, serializer: SafeUserSerializer
 
   has_many :comments, attributes: [:created_at, :updated_at, :body, :edited, :deleted_at, :deleted_by, :deleted], serializer: CommentSerializer do |serializer, params|
     if params[:current_user]&.moderator?
