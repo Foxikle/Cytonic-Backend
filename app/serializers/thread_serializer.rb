e0b7a2a2822abd9b1186971b2faf5f568e407f67
foxikle <tristantroop260@gmail.com>
2024-06-04 17:12:21 -0400

    Fix leaking emails addresses in threads

--- app/serializers/thread_serializer.rb
+++ app/serializers/thread_serializer.rb
@@ -4,7 +4,7 @@ class ThreadSerializer
 
   attributes :id, :private, :title, :body, :user, :topic, :category, :created_at, :updated_at, :deleted, :deleted_at, :edited, :deleted_at, :locked, :locked_at
 
-  belongs_to :user, serializer: UserSerializer
+  belongs_to :user, serializer: SafeUserSerializer
 
   has_many :comments, attributes: [:created_at, :updated_at, :body, :edited, :deleted_at, :deleted_by, :deleted], serializer: CommentSerializer do |serializer, params|
     if params[:current_user]&.moderator?
