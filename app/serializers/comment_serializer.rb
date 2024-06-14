c607befb5fe4a2d2cc4c00a2a37eda93cc6f755f
foxikle <tristantroop260@gmail.com>
2024-06-14 18:21:57 -0400

    give more info about deleted comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,10 +1,18 @@
 class CommentSerializer
   include JSONAPI::Serializer
 
-  attributes :created_at, :updated_at, :body, :edited, :deleted_at, :deleted_by, :deleted  # Fields to include in serialization
+  attributes :created_at, :updated_at, :body, :edited, :deleted_at, :deleted # Fields to include in serialization
 
   attribute :user do |comment|
     SafeUserSerializer.new(comment.user).serializable_hash
+  end
+
+  attribute :deleted_by do |comment|
+    if comment.deleted
+      SafeUserSerializer.new(User.find(comment.deleted_by)).serializable_hash
+    else
+      nil
+    end
   end
 
   belongs_to :user, serializer: UserSerializer
